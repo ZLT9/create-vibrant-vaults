19af5b9b67e6ec4aa4ef9d8344a31156a49f606e
ZLT <manult002@gmail.com>
2025-03-12 16:49:13 -0300

    feature add ponder scenes

---
+++ src/main/java/net/zlt/create_vibrant_vaults/mixin/client/AllCreatePonderScenesMixin.java
@@ -0,0 +1,62 @@
+package net.zlt.create_vibrant_vaults.mixin.client;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.simibubi.create.infrastructure.ponder.AllCreatePonderScenes;
+import com.simibubi.create.infrastructure.ponder.AllCreatePonderTags;
+import com.simibubi.create.infrastructure.ponder.scenes.ItemVaultScenes;
+import com.simibubi.create.infrastructure.ponder.scenes.highLogistics.FrogAndConveyorScenes;
+import com.simibubi.create.infrastructure.ponder.scenes.highLogistics.PackagerScenes;
+import com.simibubi.create.infrastructure.ponder.scenes.highLogistics.RequesterAndShopScenes;
+import com.simibubi.create.infrastructure.ponder.scenes.highLogistics.StockLinkScenes;
+import com.tterrag.registrate.util.entry.ItemProviderEntry;
+import net.createmod.ponder.api.registration.PonderSceneRegistrationHelper;
+import net.minecraft.resources.ResourceLocation;
+import net.zlt.create_vibrant_vaults.CreateVibrantVaults;
+import net.zlt.create_vibrant_vaults.block.ModBlocks;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mixin(AllCreatePonderScenes.class)
+public abstract class AllCreatePonderScenesMixin {
+    @Unique
+    private static PonderSceneRegistrationHelper<ItemProviderEntry<?>> createVibrantVaults$helper = null;
+
+    @ModifyExpressionValue(method = "register", at = @At(value = "INVOKE", target = "Lnet/createmod/ponder/api/registration/PonderSceneRegistrationHelper;withKeyFunction(Ljava/util/function/Function;)Lnet/createmod/ponder/api/registration/PonderSceneRegistrationHelper;"), remap = false)
+    private static PonderSceneRegistrationHelper<ItemProviderEntry<?>> createVibrantVaults$grabPonderHelper(PonderSceneRegistrationHelper<ItemProviderEntry<?>> original) {
+        createVibrantVaults$helper = original;
+        return original;
+    }
+
+    @Inject(method = "register", at = @At(value = "TAIL"), remap = false)
+    private static void createVibrantVaults$addPonderScenes(PonderSceneRegistrationHelper<ResourceLocation> helper, CallbackInfo ci) {
+        if (createVibrantVaults$helper == null) {
+            CreateVibrantVaults.LOGGER.warn("Failed to add ponder scenes");
+            return;
+        }
+
+        createVibrantVaults$helper.forComponents(ModBlocks.VIBRANT_VAULTS.stream().flatMap(List::stream).collect(Collectors.toList()))
+            .addStoryBoard("item_vault/storage", ItemVaultScenes::storage, AllCreatePonderTags.LOGISTICS)
+            .addStoryBoard("item_vault/sizes", ItemVaultScenes::sizes);
+
+        createVibrantVaults$helper.forComponents(ModBlocks.VIBRANT_FROGPORTS)
+            .addStoryBoard("high_logistics/package_frogport", FrogAndConveyorScenes::frogPort);
+
+        createVibrantVaults$helper.forComponents(ModBlocks.VIBRANT_PACKAGERS)
+            .addStoryBoard("high_logistics/packager", PackagerScenes::packager)
+            .addStoryBoard("high_logistics/packager_address", PackagerScenes::packagerAddress);
+
+        createVibrantVaults$helper.forComponents(ModBlocks.VIBRANT_STOCK_LINKS)
+            .addStoryBoard("high_logistics/stock_link", StockLinkScenes::stockLink);
+
+        createVibrantVaults$helper.forComponents(ModBlocks.VIBRANT_REDSTONE_REQUESTERS)
+            .addStoryBoard("high_logistics/redstone_requester", RequesterAndShopScenes::requester);
+
+        createVibrantVaults$helper = null;
+    }
+}
