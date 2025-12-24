119e2ae95488702364b60f86553fc8495ba2a352
swzo <omarahmedmmsalem@gmail.com>
2025-12-24 19:21:28 +0200

    initialize partial models in client constructor to prevent render crash

--- src/main/java/net/zlt/create_vibrant_vaults/client/CreateVibrantVaultsClient.java
+++ src/main/java/net/zlt/create_vibrant_vaults/client/CreateVibrantVaultsClient.java
@@ -10,10 +10,11 @@ import net.zlt.create_vibrant_vaults.client.model.ModPartialModels;
 @Mod(value = CreateVibrantVaults.ID, dist = Dist.CLIENT)
 public class CreateVibrantVaultsClient {
     public CreateVibrantVaultsClient(IEventBus modEventBus) {
+        // Call init immediately in the constructor to ensure partial mmodels are registered before the Model Baking phase starts.
+        ModPartialModels.init();
         modEventBus.addListener(CreateVibrantVaultsClient::clientInit);
     }
 
     public static void clientInit(final FMLClientSetupEvent event) {
-        ModPartialModels.init();
     }
 }
